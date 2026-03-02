5799ecd678511b04d9c6cd0d17eb8ae997a6d8be
Warren Afable <afablewarren2004@gmail.com>
2026-03-02 10:58:57 +0800

    fixed images in gallery

--- views/gallery.php
+++ views/gallery.php
@@ -25,47 +25,47 @@
         <div class="parent">
 
             <div class="div1 gallery-item">
-                <img src="static/images/lapaseo_bar.jpg">
+                <img src="static/img/lapaseo_bar.jpg">
                 <div class="caption">Barr Mont Le Paseo Resort</div>
             </div>
 
             <div class="div2 gallery-item">
-                <img src="static/images/sea_view.jpg">
+                <img src="static/img/sea_view.jpg">
                 <div class="caption">Ocean View</div>
             </div>
 
             <div class="div3 gallery-item">
-                <img src="static/images/cottage.jpg">
+                <img src="static/img/cottage.jpg">
                 <div class="caption">Resort Cottage</div>
             </div>
 
             <div class="div4 gallery-item">
-                <img src="static/images/a_frame.jpg">
+                <img src="static/img/a_frame.jpg">
                 <div class="caption">Beach Villa</div>
             </div>
 
             <div class="div5 gallery-item">
-                <img src="static/images/bar_side.jpg">
+                <img src="static/img/bar_side.jpg">
                 <div class="caption">Family Room</div>
             </div>
 
             <div class="div6 gallery-item">
-                <img src="static/images/bird_house.jpg">
+                <img src="static/img/bird_house.jpg">
                 <div class="caption">Beach Sunset</div>
             </div>
 
             <div class="div7 gallery-item">
-                <img src="static/images/relaxing_port.jpg">
+                <img src="static/img/relaxing_port.jpg">
                 <div class="caption">Extra Image</div>
             </div>
 
             <div class="div8 gallery-item">
-                <img src="static/images/cottage.jpg">
+                <img src="static/img/volleyball.jpg">
                 <div class="caption">Extra Image</div>
             </div>
-
+    
             <div class="div9 gallery-item">
-                <img src="static/images/cottage.jpg">
+                <img src="static/img/bar_side.jpg">
                 <div class="caption">Extra Image</div>
             </div>
 

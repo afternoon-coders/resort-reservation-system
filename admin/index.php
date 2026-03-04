e5897f7aad368bacf9cc5c116b4f2b5a6f3ae372
xespa <markchristianespanol1016@gmail.com>
2026-03-04 20:27:48 +0800

    Added pages such as reservations, rooms, customers, reports

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,15 @@
 <?php
 // Determine which page to load
 $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
-$allowed_pages = ['dashboard', 'add_room', 'edit_room', 'manage_rooms'];
+$allowed_pages = ['dashboard', 
+                  'add_room', 
+                  'edit_room', 
+                  'manage_rooms', 
+                  'reservations',
+                  'rooms',
+                  'customers',
+                  'reports',
+                  ];
 
 if (!in_array($page, $allowed_pages)) {
     $page = 'dashboard';

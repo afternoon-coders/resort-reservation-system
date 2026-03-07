2bece5853d0e1d22df260eab436e461cae379bc0
xespa <markchristianespanol1016@gmail.com>
2026-03-07 21:04:45 +0800

    Added a favicon

--- inc/header.php
+++ inc/header.php
@@ -14,6 +14,7 @@ $user = $isLoggedIn ? getCurrentUser() : null;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="icon" type="image/png" href="/static/img/lepaseo_logo.png">
     <link rel="stylesheet" href="/static/css/style.css">
     <link rel="stylesheet" href="/static/css/adminpanel.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
